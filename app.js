272ea25b046928f2a431a227c6666d4906e2446c
Lucky Kashyap <kashyapkrlucky@gmail.com>
2021-03-20 16:12:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// Import modules
+const express = require('express');
+const PORT = 3000;
+// express app defintion
+const app = express();
+
+// Setting database
+require('./db');
+
+// Setting middleware
+require('./middleware')(app);
+
+// listening app
+app.listen(PORT, () => {
+    console.log(`Your server is running on PORT ${PORT}`);
+});
