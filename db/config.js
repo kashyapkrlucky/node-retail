e6bcc97b1aaa2f9aa4a477c5e72f638378fb6697
Lucky Kashyap <kashyapkrlucky@gmail.com>
2021-03-22 16:21:45 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,3 +1,4 @@
-module.exports ={
-    server: "mongodb+srv://kashyapkrlucky:9936058527@cluster0.ftzrr.mongodb.net/retail?retryWrites=true&w=majority"
+const pass = Buffer.from('OTkzNjA1ODUyNw==', "base64").toString("ascii");
+module.exports = {
+    server: `mongodb+srv://kashyapkrlucky:${pass}@cluster0.ftzrr.mongodb.net/retail?retryWrites=true&w=majority`
 }
