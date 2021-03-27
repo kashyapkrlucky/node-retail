dbc1d6d999d2c6070314b29c71bfe99e58d802c7
Lucky Kashyap <kashyapkrlucky@gmail.com>
2021-03-27 17:25:01 +0530

    Update authentication.js

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -19,15 +19,56 @@ exports.validate = (req, res, next) => {
     }
 }
 
-// Validate User
+// Validate User by Id
 exports.validateUser = (req, res, next) => {
     const bearer = req['headers']['authorization'];
     if (bearer) {
         const token = bearer.split(' ')[1];
         jwt.verify(token, secret, function (err, value) {
             if (value) {
-                console.log(value.id, req.params.id);
                 if (value.id === req.params.id) {
+                    next();
+                } else {
+                    unauthorize(res, '', 'You are not authorize to view');
+                }
+            } else {
+                unauthorize(res, '', 'Invalid Token');
+            }
+        });
+    } else {
+        unauthorize(res, '', 'You are not authorize to view');
+    }
+}
+
+// Validate Vendor User
+exports.isCustomerUser = (req, res, next) => {
+    const bearer = req['headers']['authorization'];
+    if (bearer) {
+        const token = bearer.split(' ')[1];
+        jwt.verify(token, secret, function (err, value) {
+            if (value) {
+                if (value.type === 'customer') {
+                    next();
+                } else {
+                    unauthorize(res, '', 'You are not authorize to view');
+                }
+            } else {
+                unauthorize(res, '', 'Invalid Token');
+            }
+        });
+    } else {
+        unauthorize(res, '', 'You are not authorize to view');
+    }
+}
+
+// Validate Vendor User
+exports.isVendorUser = (req, res, next) => {
+    const bearer = req['headers']['authorization'];
+    if (bearer) {
+        const token = bearer.split(' ')[1];
+        jwt.verify(token, secret, function (err, value) {
+            if (value) {
+                if (value.type === 'vendor') {
                     next();
                 } else {
                     unauthorize(res, '', 'You are not authorize to view');
